6f7901d8847eb659d940b6d3026117dfd387150d
root <root@spxxxx.serverpronto.com>
2013-06-18 22:13:11 -0400

    Change the PUBKEY_ADDRESS value

--- src/base58.h
+++ src/base58.h
@@ -278,7 +278,7 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 48, // Litecoin addresses start with L
+        PUBKEY_ADDRESS = 42, // Blackcoin addresses start with B
         SCRIPT_ADDRESS = 5,
         PUBKEY_ADDRESS_TEST = 111,
         SCRIPT_ADDRESS_TEST = 196,
