2129c4e72719dfbbe19e2ed43246a2a092803786
Rafael Marconi <rmmarconi@gmail.com>
2013-06-19 09:08:02 -0300

    Minor messages updates

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("BLC");
+    case mBTC: return QString("mBLC");
+    case uBTC: return QString::fromUtf8("μBLC");
     default: return QString("???");
     }
 }
